165b370170e24d1735bc2affa672874aade53005
Murat Çeliker <murat@serenay.net.tr>
2026-08-20 16:27:33 +0300

    kitty-taskbar 1.0.5

--- Casks/kitty-taskbar.rb
+++ Casks/kitty-taskbar.rb
@@ -1,6 +1,6 @@
 cask "kitty-taskbar" do
-  version "1.0.4"
-  sha256 "3d1e7ce043aff63e36d70ce5d39a8ed38e2744833dbecdb29585e5003cd4e830"
+  version "1.0.5"
+  sha256 "d0a5917567e9622811a9ccf0b63d899d044325bff11ea39b6f6a2dd2964bef6f"
 
   url "https://github.com/muarifer/kitty-taskbar/releases/download/v#{version}/KittyTaskbar-#{version}.zip"
   name "KittyTaskbar"
